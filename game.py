a5f9f4173d14145f2630f1c85144b9feeb53e114
karankreation <kangsaab5248@gmail.com>
2021-03-27 18:49:44 +0530

    LOOP FUNCTIONALITY AND DESIGN

--- game.py
+++ game.py
@@ -4,33 +4,87 @@
 from random import randint
 choices = ["rock", "paper", "scissors"]
 
-#player_choice = choices[1] 
+player_lives = 3
+computer_lives = 3
+total_lives = 3
 
-#print("index 1 in the choice array is " + player_choice+ ",which is paper")
 
-player_choice = input("choose rock, paper, or scissors: ")
-print("user chose: " + player_choice)
+player_choice = False
 
-computer_choice = choices[randint(0, 2)]
-print("computer chose: " + computer_choice)
 
-if computer_choice == player_choice: 
-    print("tie")
-    
-elif computer_choice == "rock":
-    if player_choice == "scissors":
-        print("you lose!")
+def winorlose(status):
+    #print("inside winorlose function; status is: ",status)
+    print("You", status, "! Would you like to play again?")
+    choice = input("Y / N? ")
+
+    if choice == "N" or choice == "n":
+        print("you chose to quit! Better luck next time!")
+        exit()
+    elif choice == "Y" or choice == "y":
+
+
+        global player_lives
+        global computer_lives
+        global total_lives
+
+        player_lives = total_lives
+        computer_lives = total_lives
     else:
-        print("you win!")
+        print("Make a valid choice - Y or N")
+        choice = input("Y / N?")
 
-elif computer_choice == "paper":
-    if player_choice == "scissors":
-        print("you win!")
-    else:
-        print("you lose!")
+while player_choice is False:
+    print("=======================*/ RPS GAME */=======================")
+    print("Computer Lives:", computer_lives, "/", total_lives)
+    print("Player Lives:", player_lives, "/", total_lives)
+    print("============================================================")
+
+
+
+    player_choice = input("choose rock, paper, or scissors: \n ")
+    print("user chose: " + player_choice)
+
+    computer_choice = choices[randint(0, 2)]
+    print("computer chose: " + computer_choice)
+
+    if computer_choice == player_choice: 
+        print("tie")
         
-elif computer_choice == "scissors":
-    if player_choice == "paper":
-        print("you lose!")
-    else: 
-        print("you win!")
+    elif computer_choice == "rock":
+        if player_choice == "scissors":
+            print("you lose!")
+            player_lives -= 1
+        else:
+            print("you win!")
+            computer_lives -= 1
+
+    elif computer_choice == "paper":
+        if player_choice == "scissors":
+            print("you win!")
+            computer_lives -= 1
+
+        else:
+            print("you lose!")
+            player_lives -= 1
+            
+    elif computer_choice == "scissors":
+        if player_choice == "paper":
+            print("you lose!")
+            player_lives -= 1
+        else: 
+            print("you win!")  
+            computer_lives -= 1
+    
+   
+    if player_lives == 0:
+        winorlose("lose")
+    
+    if computer_lives == 0:
+        winorlose("won")
+        
+    
+    print("player lives:", player_lives)
+    print("computer lives:", computer_lives)
+
+
+    player_choice = False
